9d8de20d78cf7aa76511b5a47d32f66b0627860e
rudylpzjme <rudylpzjme@gmail.com>
2020-01-15 16:02:33 -0600

    Updates

---
+++ precache-manifest.6229c65c3c6a4b235ba35aa0d04bf3fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49f3496f74971cca18abf80c8710afc8",
+    "url": "/monsters-app/index.html"
+  },
+  {
+    "revision": "872ff1359a7e6523c4ba",
+    "url": "/monsters-app/static/css/main.b2b74cf8.chunk.css"
+  },
+  {
+    "revision": "d3c64bfb20f3a5e9d281",
+    "url": "/monsters-app/static/js/2.e23fb1fd.chunk.js"
+  },
+  {
+    "revision": "872ff1359a7e6523c4ba",
+    "url": "/monsters-app/static/js/main.9029bb16.chunk.js"
+  },
+  {
+    "revision": "7dd112df68b73dfd9793",
+    "url": "/monsters-app/static/js/runtime-main.97d1269e.js"
+  }
+]);
